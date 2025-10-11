571c16511027ca2337bdcefa8ebdd35f5c546fdf
Joshua Davies <cinchtail@gmail.com>
2025-10-11 17:41:31 -0400

    json files fixed

---
+++ src/main/java/net/cinchtail/cinchstoolvariants/CinchsToolVariants.java
@@ -0,0 +1,16 @@
+package net.cinchtail.cinchstoolvariants;
+
+import net.cinchtail.cinchstoolvariants.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CinchsToolVariants implements ModInitializer {
+	public static final String MOD_ID = "cinchstoolvariants";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+	}
+}
---
+++ src/main/java/net/cinchtail/cinchstoolvariants/item/ModItemGroups.java
@@ -0,0 +1,12 @@
+package net.cinchtail.cinchstoolvariants.item;
+
+import net.cinchtail.cinchstoolvariants.CinchsToolVariants;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup CINCHSTOOLVARIANTS_TAB = FabricItemGroupBuilder.build(
+            new Identifier(CinchsToolVariants.MOD_ID, "cinchstoolvariants_tab"), () -> new ItemStack(ModItems.DEEPSLATE_PICKAXE));
+}
