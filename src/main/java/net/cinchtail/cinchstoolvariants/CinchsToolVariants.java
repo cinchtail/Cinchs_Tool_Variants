7c35e38e8ff7163e336e84a6d7bad79c1fe9dfe2
Joshua Davies <cinchtail@gmail.com>
2025-12-01 19:28:37 -0500

    all added

---
+++ src/main/java/net/cinchtail/cinchstoolvariants/CinchsToolVariants.java
@@ -0,0 +1,18 @@
+package net.cinchtail.cinchstoolvariants;
+
+import net.cinchtail.cinchstoolvariants.item.ModItemGroups;
+import net.cinchtail.cinchstoolvariants.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CinchsToolVariants implements ModInitializer {
+	public static final String MOD_ID = "cinchstoolvariants";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+	}
+}
