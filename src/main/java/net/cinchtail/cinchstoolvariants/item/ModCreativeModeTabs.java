4700d99fefa7572ad878185fbdceed7246241d93
Joshua Davies <cinchtail@gmail.com>
2026-01-04 04:38:47 -0500

    icon and tweaks

--- src/main/java/net/cinchtail/cinchstoolvariants/item/ModCreativeModeTabs.java
+++ src/main/java/net/cinchtail/cinchstoolvariants/item/ModCreativeModeTabs.java
@@ -6,7 +6,7 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
-public class ModItemGroups {
+public class ModCreativeModeTabs {
     public static final ItemGroup CINCHSTOOLVARIANTS_TAB = FabricItemGroupBuilder.build(
             new Identifier(CinchsToolVariants.MOD_ID, "cinchstoolvariants_tab"), () -> new ItemStack(ModItems.DEEPSLATE_PICKAXE));
 }
