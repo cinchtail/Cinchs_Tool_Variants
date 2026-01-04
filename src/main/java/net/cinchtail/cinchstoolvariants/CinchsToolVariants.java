15b61dc509bb32e296a87bfbb0a14518ea21303f
Joshua Davies <cinchtail@gmail.com>
2026-01-04 07:41:56 -0500

    all added

---
+++ src/main/java/net/cinchtail/cinchstoolvariants/CinchsToolVariants.java
@@ -0,0 +1,20 @@
+package net.cinchtail.cinchstoolvariants;
+
+import net.cinchtail.cinchstoolvariants.item.ModCreativeModeTabs;
+import net.cinchtail.cinchstoolvariants.item.ModItems;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.LoggerFactory;
+
+@Mod(CinchsToolVariants.MOD_ID)
+public class CinchsToolVariants {
+    public static final String MOD_ID = "cinchstoolvariants";
+    public static final Logger LOGGER = LoggerFactory.getLogger(CinchsToolVariants.class);
+
+    public CinchsToolVariants(@NotNull IEventBus bus) {
+        ModItems.ITEMS.register(bus);
+        ModCreativeModeTabs.CREATIVE_MODE_TABS.register(bus);
+    }
+}
