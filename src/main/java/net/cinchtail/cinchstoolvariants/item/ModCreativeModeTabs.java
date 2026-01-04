4f1804ed9651df906dbaf1424a1e10bdd1c406f5
Joshua Davies <cinchtail@gmail.com>
2026-01-04 06:58:43 -0500

    all added

---
+++ src/main/java/net/cinchtail/cinchstoolvariants/item/ModCreativeModeTabs.java
@@ -0,0 +1,33 @@
+package net.cinchtail.cinchstoolvariants.item;
+
+import net.cinchtail.cinchstoolvariants.CinchsToolVariants;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModCreativeModeTabs {
+    public static final ItemGroup CINCHSTOOLVARIANTS_TAB = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(CinchsToolVariants.MOD_ID, "cinchstoolvariants"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.cinchstoolvariants"))
+                    .icon(() -> new ItemStack(ModItems.DEEPSLATE_PICKAXE)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.DEEPSLATE_SWORD);
+                        entries.add(ModItems.DEEPSLATE_PICKAXE);
+                        entries.add(ModItems.DEEPSLATE_AXE);
+                        entries.add(ModItems.DEEPSLATE_SHOVEL);
+                        entries.add(ModItems.DEEPSLATE_HOE);
+                        entries.add(ModItems.BLACKSTONE_SWORD);
+                        entries.add(ModItems.BLACKSTONE_PICKAXE);
+                        entries.add(ModItems.BLACKSTONE_AXE);
+                        entries.add(ModItems.BLACKSTONE_SHOVEL);
+                        entries.add(ModItems.BLACKSTONE_HOE);
+                    }).build());
+
+
+    public static void registerItemGroups() {
+        CinchsToolVariants.LOGGER.info("Registering Item Groups for " + CinchsToolVariants.MOD_ID);
+    }
+}
