31838b93bf31414401318b8a7813edf5a54f5cc0
Joshua Davies <cinchtail@gmail.com>
2026-01-05 23:06:51 -0500

    all added

---
+++ src/main/java/net/cinchtail/cinchstoolvariants/util/ModTags.java
@@ -0,0 +1,20 @@
+package net.cinchtail.cinchstoolvariants.util;
+
+import net.cinchtail.cinchstoolvariants.CinchsToolVariants;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+
+public class ModTags {
+    public static class Items {
+
+        public static final TagKey<Item> DEEPSLATE_REPAIR = createTag("deepslate_repair");
+        public static final TagKey<Item> BLACKSTONE_REPAIR = createTag("blackstone_repair");
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, Identifier.of(CinchsToolVariants.MOD_ID, name));
+        }
+    }
+}
